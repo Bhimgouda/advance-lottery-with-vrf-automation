33f612c6822274995b0f2e5e032d1f164d72ae65
Bhimgouda <satvikp508@gmail.com>
2023-04-11 09:39:48 +0530

    added unit and staging tests

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,68 +2,111 @@
 
 pragma solidity ^0.8.7;
 
-// As an interface to call functions through contract instance
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-// To Inherit functionality
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+import "hardhat/console.sol";
 
-// For chainlink keeper
-import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
-
-error Lottery__NotEnoughEthEntered();
+/* Errors */
+error Lottery__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 lotteryState);
 error Lottery__TransferFailed();
-error Lottery__NotOpen();
-error Lottery__UpkeepNotNeeded(uint256 currentBalance, uint numPlayers, uint256 lotteryState);
+error Lottery__SendMoreToEnterLottery();
+error Lottery__LotteryNotOpen();
 
-/**
- * @title A advanced lottery Contract
- * @author Bhimgouda D Patil
- * @notice This contract is for creating an untamperable decentralized samrt contract
- * @dev This implements Chainlink VRF v2 and chainlink keepers for contract Automation
+/**@title A sample Lottery Contract
+ * @author Bhimgouda Patil
+ * @notice This contract is for creating a sample lottery contract
+ * @dev This implements the Chainlink VRF Version 2
  */
 contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
-    // Type declarations
+    /* Type declarations */
     enum LotteryState {
         OPEN,
         CALCULATING
     }
-
-    // State Variables
-    uint256 private immutable i_entranceFee;
-    address payable[] private s_players;
+    /* State variables */
+    // Chainlink VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
-    bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
-    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    bytes32 private immutable i_gasLane;
     uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
     // Lottery Variables
-    address private s_recentWinner;
-    LotteryState private s_lotteryState;
-    uint256 private s_lastTimeStamp;
     uint256 private immutable i_interval;
+    uint256 private immutable i_entranceFee;
+    uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+    address payable[] private s_players;
+    LotteryState private s_lotteryState;
 
-    // Events
-    event EnteredLottery(address indexed player);
+    /* Events */
     event RequestedLotteryWinner(uint256 indexed requestId);
-    event WinnerPicked(address indexed winner);
+    event LotteryEnter(address indexed player);
+    event WinnerPicked(address indexed player);
 
-    // Modifiers
-    modifier minEth() {
+    /* Functions */
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_interval = interval;
+        i_subscriptionId = subscriptionId;
+        i_entranceFee = entranceFee;
+        s_lotteryState = LotteryState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function enterLottery() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough value sent");
+        // require(s_lotteryState == LotteryState.OPEN, "Lottery is not open");
         if (msg.value < i_entranceFee) {
-            revert Lottery__NotEnoughEthEntered();
+            revert Lottery__SendMoreToEnterLottery();
         }
-        _;
-    }
-    modifier ifLotteryOpen() {
         if (s_lotteryState != LotteryState.OPEN) {
-            revert Lottery__NotOpen();
+            revert Lottery__LotteryNotOpen();
         }
-        _;
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
+        emit LotteryEnter(msg.sender);
     }
-    modifier ifUpkeepNeeded() {
+
+    /**
+     * @dev This is the function that the Chainlink Keeper nodes call
+     * they look for `upkeepNeeded` to return True.
+     * the following should be true for this to return true:
+     * 1. The time interval has passed between lottery runs.
+     * 2. The lottery is open.
+     * 3. The contract has ETH.
+     * 4. Implicity, your subscription is funded with LINK.
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public view override returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool isOpen = LotteryState.OPEN == s_lotteryState;
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = s_players.length > 0;
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers);
+        return (upkeepNeeded, "0x0"); // can we comment this out?
+    }
+
+    /**
+     * @dev Once `checkUpkeep` is returning `true`, this function is called
+     * and it kicks off a Chainlink VRF call to get a random winner.
+     */
+    function performUpkeep(bytes calldata /* performData */) external override {
         (bool upkeepNeeded, ) = checkUpkeep("");
+        // require(upkeepNeeded, "Upkeep not needed");
         if (!upkeepNeeded) {
             revert Lottery__UpkeepNotNeeded(
                 address(this).balance,
@@ -71,62 +114,7 @@ contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
                 uint256(s_lotteryState)
             );
         }
-        _;
-    }
-
-    constructor(
-        address vrfCoordinatorV2,
-        uint256 entranceFee,
-        bytes32 gasLane,
-        uint64 subscriptionId,
-        uint32 callbackGasLimit,
-        uint256 interval
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
-        i_entranceFee = entranceFee;
-        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
-        i_gasLane = gasLane;
-        i_subscriptionId = subscriptionId;
-        i_callbackGasLimit = callbackGasLimit;
-        s_lotteryState = LotteryState.OPEN;
-        s_lastTimeStamp = block.timestamp;
-        i_interval = interval;
-    }
-
-    function enterLottery() public payable minEth ifLotteryOpen {
-        s_players.push(payable(msg.sender));
-        emit EnteredLottery(msg.sender);
-    }
-
-    /**
-     * @dev This is the function that the Chinlink keeper nodes call
-     * & they look for the upkeepNeeded to return true (if given time has passed or logic passes)
-     * In our case for upKeepNeeded to be true we need
-     * 1. Our time interval should have passes
-     * 2. The lottery should have at least 1 player, and have some ETH
-     * 3. Our subscription is funded with LINK
-     * 4. The Lottery should be in an Open state
-     */
-    function checkUpkeep(
-        bytes memory /*checkData*/
-    ) public override returns (bool upkeepNeeded, bytes memory) {
-        //
-        bool isOpen = (s_lotteryState == LotteryState.OPEN);
-        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
-        bool hasPlayers = (s_players.length > 0);
-        bool hasBalance = address(this).balance > 0;
-        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
-    }
-
-    // After checkup keep returns true the performUpkeep will be called
-
-    // To get a really big random numbers array/words array but oru array will be of size 1 as we have set NUM_WORDS=1
-    function performUpkeep(bytes memory /* performData */) external override ifUpkeepNeeded {
-        // Just performing an extra check if someone else calls by a modifier attached
-
         s_lotteryState = LotteryState.CALCULATING;
-
-        // Request the random number
-        // Once we get it, do something with it
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
@@ -134,41 +122,39 @@ contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
             i_callbackGasLimit,
             NUM_WORDS
         );
+        // Quiz... is this redundant?
         emit RequestedLotteryWinner(requestId);
     }
 
+    /**
+     * @dev This is the function that Chainlink VRF node
+     * calls to send the money to the random winner.
+     */
     function fulfillRandomWords(
         uint256 /* requestId */,
         uint256[] memory randomWords
     ) internal override {
-        // using modulo of players array to get random number b/w that range from a
-        // really huge random number that we get from VRF
+        // s_players size 10
+        // randomNumber 202
+        // 202 % 10 ? what's doesn't divide evenly into 202?
+        // 20 * 10 = 200
+        // 2
+        // 202 % 10 = 2
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
-        s_lotteryState = LotteryState.OPEN;
         s_players = new address payable[](0);
-        // Sending the winner contract balance without any data ""
+        s_lotteryState = LotteryState.OPEN;
+        s_lastTimeStamp = block.timestamp;
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        // require(success, "Transfer failed");
         if (!success) {
             revert Lottery__TransferFailed();
         }
         emit WinnerPicked(recentWinner);
     }
 
-    // ------------- View and Pure --------------- //
-
-    function getEntranceFee() public view returns (uint256) {
-        return i_entranceFee;
-    }
-
-    function getPlayer(uint256 index) public view returns (address) {
-        return s_players[index];
-    }
-
-    function getRecentWinner() public view returns (address) {
-        return s_recentWinner;
-    }
+    /** Getter Functions */
 
     function getLotteryState() public view returns (LotteryState) {
         return s_lotteryState;
@@ -178,19 +164,31 @@ contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
         return NUM_WORDS;
     }
 
-    function getNumberOfPlayers() public view returns (uint256) {
-        return s_players.length;
-    }
-
     function getRequestConfirmations() public pure returns (uint256) {
         return REQUEST_CONFIRMATIONS;
     }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getLastTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
 }
-
-// Objectives
-
-// Enter the lottery (paying some amount)
-// Pick a random winner (verifiably random)
-// Winner to be selected every X minutes -> completely automated
-
-// Chainlink Oracle -> Randomness, Automated Execution (Chainlink keepers)
--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,58 +1,69 @@
 const { network, ethers } = require("hardhat")
-const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
+const FUND_AMOUNT = ethers.utils.parseEther("1") // 1 Ether, or 1e18 (10^18) Wei
 
-module.exports = async function ({ getNamedAccounts, deployments }) {
+module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts
+    const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
 
-    let vrfCoordinatorV2Address
-    let subscriptionId
-
-    if (developmentChains.includes(network.name)) {
-        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoodinatorV2Mock")
-        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
-
-        // Creating subscription, getting subs id, and funding the subs "all programmatically"
-        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
-        const transactionReceipt = await transactionResponse.wait(1)
-
+    if (chainId == 31337) {
+        // create VRFV2 Subscription
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait()
         subscriptionId = transactionReceipt.events[0].args.subId
-
-        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+        // Fund the subscription
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
         subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
 
-    const entranceFee = networkConfig[chainId]["entranceFee"]
-    const gasLane = networkConfig[chainId]["gasLane"]
-    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
-    const interval = networkConfig[chainId]["interval"]
-
-    const args = [
+    log("----------------------------------------------------")
+    const arguments = [
         vrfCoordinatorV2Address,
-        entranceFee,
-        gasLane,
         subscriptionId,
-        callbackGasLimit,
-        interval,
+        networkConfig[chainId]["gasLane"],
+        networkConfig[chainId]["keepersUpdateInterval"],
+        networkConfig[chainId]["lotteryEntranceFee"],
+        networkConfig[chainId]["callbackGasLimit"],
     ]
-
     const lottery = await deploy("Lottery", {
         from: deployer,
-        args,
-        logs: true,
-        waitConfirmations: network.config.blockConfirmations || 1,
+        args: arguments,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
     })
 
+    // Ensure the Lottery contract is a valid consumer of the VRFCoordinatorV2Mock contract.
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, lottery.address)
+    }
+
+    // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
-        await verify(lottery.address, args)
+        await verify(lottery.address, arguments)
     }
-    log("------------------------------------")
+
+    log("Enter lottery with command:")
+    const networkName = network.name == "hardhat" ? "localhost" : network.name
+    log(`yarn hardhat run scripts/enterLottery.js --network ${networkName}`)
+    log("----------------------------------------------------")
 }
 
 module.exports.tags = ["all", "lottery"]
